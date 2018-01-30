33ff01575f39724875e5b4dfd250cb590d77229e
Casey Chan <caseygocoding@gmail.com>
2018-01-30 15:23:10 -0500

    tripplanner

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker.js");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FzZXlnb2NvZGluZyIsImEiOiJjamQxd3NrZjcwamQ0MndwYTFqcGN1MjdpIn0.5YFmc0pCXzAqd28r-IUNJQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+marker.addTo(map);
+
+
